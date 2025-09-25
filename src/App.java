7e9bddaa9b742416aed0e866a313d9b0c41962d5
Gabriel Vitor da Silva <gabriel_silva215@estudante.sesisenai.org.br>
2025-09-25 20:12:20 -0300

    TESTE
    TESTE

--- src/App.java
+++ src/App.java
@@ -1,7 +1,12 @@
 public class App {
     public static void main(String[] args) throws Exception {
         for(;;){
-            
+
         }
-    }
+        while(true) {
+            
+        };
+
+}
+
 }
